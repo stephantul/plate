a687d2190e76949dd4d8a230eda4079dc81904ad
stephantul <stephan.tulkens@uantwerpen.be>
2019-06-20 21:41:16 +0200

    add fast version of circular convolution

--- plate/plate.py
+++ plate/plate.py
@@ -15,16 +15,8 @@ def addition(p):
 
 @memoize
 def circular_convolution(x, y):
-    """Make a fast version."""
-    if np.ndim(x) == 1:
-        x = x[None, :]
-    if np.ndim(y) == 1:
-        y = y[None, :]
-    assert(x.shape == y.shape)
-    length = x.shape[1]
-    vec = np.arange(length)
-    mtr = (vec[None, :] - vec[:, None]) % length
-    return (x[:, :, None] * y[:, mtr]).sum(-2)
+    """A fast version of the circular convolution."""
+    return np.fft.ifft(np.fft.fft(x) * np.fft.fft(y)).real
 
 
 def involution(x):
